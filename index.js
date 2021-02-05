866b102a912e62d9478d885dabb577e62020240a
HCaetano <hrcgeo@gmail.com>
2021-02-05 18:44:14 -0300

    Exercicio do grupo 12

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
-app.get('/btc/prices', (req, res) => {
-  console.log(req.header)
+function tokenValidator(req, res, next) {
+  const token = req.headers.authorization;
+  const validator = /^[a-zA-Z0-9_.-]*$/i;
 
-  res.json(data);
+  if (token.length !== 12 || !validator.test(String(token).toLowerCase())) {
+    return res.status(401).send("Invalid token");
+  } else {
+    res.status(202).send("Successfully logged in");
+  }
+
+  next();
+}
+
+app.use(tokenValidator);
+
+async function api() {
+  const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice/BTC.json");
+  const data = await response.json().then((response) => response.bpi);
+
+  return data;
+}
+
+app.get('/btc/prices', async (req, res) => {
+  const result = await api();
+  console.log(result)
+
+  res.json(req.headers.authorization);
 });
 
 app.listen(port, () => console.log(`Example app listening on port 3000!`)); 
